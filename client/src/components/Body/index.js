cd6b450d518cda4aeb61f20c60cbbee08e0f6186
Charlie <Charlieyang1040@hotmail.com>
2021-03-18 23:13:29 +1300

    Created body component

--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,46 +1,17 @@
-<<<<<<< HEAD
-import { Grid, Paper, makeStyles } from "@material-ui/core";
-import SelectedWidgetsModal from "../SelectWidgetsModal";
-import styles from "./style.module.scss";
-import createPersistedState from "use-persisted-state";
-// import { useEffect } from "react";
-
-const useWidgetState = createPersistedState("selectedWidgets");
-
-const useStyles = makeStyles({
-    grid: {
-        width: "80%",
-    },
-    label: {
-        textTransform: "capitalize",
-        fontSize: "1.25rem",
-=======
 import { Grid, makeStyles } from "@material-ui/core";
 import styles from "./style.module.scss";
 
 const useStyles = makeStyles({
     grid: {
         padding: "20px 200px",
->>>>>>> 9deb998... Added styling to format grid item placement
     },
 });
 
 function Body() {
-<<<<<<< HEAD
-    const [selectedWidgets, setSelectedWidgets] = useWidgetState([]);
-
-    // useEffect(() => {
-    //     setSelectedWidgets(["To Do List", "Calander"]);
-    //     // eslint-disable-next-line
-    // }, []);
-
-=======
->>>>>>> 9deb998... Added styling to format grid item placement
     const classes = useStyles();
 
     return (
         <div className={styles.gridContainer}>
-<<<<<<< HEAD
             {selectedWidgets.length > 0 ? (
                 <Grid container justify="center" spacing={4} className={classes.grid}>
                     {selectedWidgets.map((value) => (
@@ -60,9 +31,7 @@ function Body() {
                     </div>
                 </div>
             )}
-=======
             <Grid container spacing={2} className={classes.grid}></Grid>
->>>>>>> 9deb998... Added styling to format grid item placement
         </div>
     );
 }
