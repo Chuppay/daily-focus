0e761a201a8c1263a99fce26c05bd68dd4b67147
Charlie <Charlieyang1040@hotmail.com>
2021-03-18 23:16:36 +1300

    Added styling to format grid item placement

--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Grid, Paper, makeStyles } from "@material-ui/core";
 import SelectedWidgetsModal from "../SelectWidgetsModal";
 import styles from "./style.module.scss";
@@ -13,10 +14,19 @@ const useStyles = makeStyles({
     label: {
         textTransform: "capitalize",
         fontSize: "1.25rem",
+=======
+import { Grid, makeStyles } from "@material-ui/core";
+import styles from "./style.module.scss";
+
+const useStyles = makeStyles({
+    grid: {
+        padding: "20px 200px",
+>>>>>>> 9deb998... Added styling to format grid item placement
     },
 });
 
 function Body() {
+<<<<<<< HEAD
     const [selectedWidgets, setSelectedWidgets] = useWidgetState([]);
 
     // useEffect(() => {
@@ -24,10 +34,13 @@ function Body() {
     //     // eslint-disable-next-line
     // }, []);
 
+=======
+>>>>>>> 9deb998... Added styling to format grid item placement
     const classes = useStyles();
 
     return (
         <div className={styles.gridContainer}>
+<<<<<<< HEAD
             {selectedWidgets.length > 0 ? (
                 <Grid container justify="center" spacing={4} className={classes.grid}>
                     {selectedWidgets.map((value) => (
@@ -47,6 +60,9 @@ function Body() {
                     </div>
                 </div>
             )}
+=======
+            <Grid container spacing={2} className={classes.grid}></Grid>
+>>>>>>> 9deb998... Added styling to format grid item placement
         </div>
     );
 }
