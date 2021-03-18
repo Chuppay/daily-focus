1a85f34b6c142e5b256e9c80a48b4a2a7fd9fc92
Charlie <Charlieyang1040@hotmail.com>
2021-03-18 23:16:36 +1300

    Added styling to format grid item placement

--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,9 +1,18 @@
 import { Grid, makeStyles } from "@material-ui/core";
+import styles from "./style.module.scss";
+
+const useStyles = makeStyles({
+    grid: {
+        padding: "20px 200px",
+    },
+});
 
 function Body() {
+    const classes = useStyles();
+
     return (
-        <div>
-            <Grid container spacing={2}></Grid>
+        <div className={styles.gridContainer}>
+            <Grid container spacing={2} className={classes.grid}></Grid>
         </div>
     );
 }
