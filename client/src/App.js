deff50389864edfff231b1aba865b1efe5a07764
Charlie <charlieyang1040@hotmail.com>
2021-03-19 02:01:06 +1300

    added conditional rednering for no widgets

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
+import Body from "./components/Body";
 import "./style.css";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/components/index.js</code> and save to reload.
-                    <br />
-                    ** DAILY FOCUS **
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Body></Body>
         </div>
     );
 }
--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,18 +1,49 @@
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, Paper, makeStyles, Button } from "@material-ui/core";
 import styles from "./style.module.scss";
 
 const useStyles = makeStyles({
+    root: {
+        background: "#30A0F5",
+        borderRadius: 50,
+        padding: "4px 25px",
+    },
     grid: {
         padding: "20px 200px",
     },
+    label: {
+        textTransform: "capitalize",
+        fontSize: "1.25rem",
+    },
 });
 
-function Body() {
+function Body(widgets) {
+    widgets = null;
     const classes = useStyles();
 
     return (
         <div className={styles.gridContainer}>
-            <Grid container spacing={2} className={classes.grid}></Grid>
+            {widgets ? (
+                <Grid container spacing={2} className={classes.grid}>
+                    {widgets.map((value) => (
+                        <Grid key={value} item>
+                            <Paper className={classes.paper} />
+                        </Grid>
+                    ))}
+                </Grid>
+            ) : (
+                <div className={styles.noWidgetContainer}>
+                    <div className={styles.message}>Your focus page is empty</div>
+                    <div className={styles.addWidget}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            classes={{ root: classes.root, label: classes.label }}
+                        >
+                            Add Widget
+                        </Button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
