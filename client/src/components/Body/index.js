f2a09a23986f1073218a6f0f072e9978dbe53cde
Charlie <Charlieyang1040@hotmail.com>
2021-03-18 23:13:29 +1300

    Created body component

---
+++ client/src/components/Body/index.js
@@ -0,0 +1,11 @@
+import { Grid, makeStyles } from "@material-ui/core";
+
+function Body() {
+    return (
+        <div>
+            <Grid container spacing={2}></Grid>
+        </div>
+    );
+}
+
+export default Body;
