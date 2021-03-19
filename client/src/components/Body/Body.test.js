1dae94d8e45f62d0bc56dbd271c0b0cf577ee91a
Charlie <charlieyang1040@hotmail.com>
2021-03-20 00:52:55 +1300

    Added extra test

--- client/src/components/Body/Body.test.js
+++ client/src/components/Body/Body.test.js
@@ -1,10 +1,14 @@
 import React from "react";
-import shallow from "enzyme"
+import { shallow } from "enzyme";
 import Body from ".";
-import renderer from "react-test-renderer"
+import renderer from "react-test-renderer";
 
-test("Renders body without crashing", () =>{
-    shallow(<Body />)
-})
+test("Renders body without crashing", () => {
+    shallow(<Body />);
+});
 
+test("Renders no widgets found message", () => {
+    const tree = renderer.create(<Body />).toJSON();
 
+    expect(tree).toMatchSnapshot();
+});
