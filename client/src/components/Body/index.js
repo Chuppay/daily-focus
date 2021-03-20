a392abc13d7a33e03f36121489e4cf246371b78e
Charlie <charlieyang1040@hotmail.com>
2021-03-21 10:37:47 +1300

    Added conditional rendering for no widgets

--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,9 +1,46 @@
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles, Paper, Button } from "@material-ui/core";
 
-function Body() {
+const useStyles = makeStyles({
+    root: {
+        background: "#30A0F5",
+        borderRadius: 50,
+        padding: "4px 25px",
+    },
+    grid: {
+        padding: "20px 200px",
+    },
+    label: {
+        textTransform: "capitalize",
+        fontSize: "1.25rem",
+    },
+});
+
+function Body(widgets) {
+    const classes = useStyles();
     return (
         <div>
-            <Grid container spacing={2}></Grid>
+            {widgets ? (
+                <Grid container spacing={2} className={classes.grid}>
+                    {widgets.map((value) => (
+                        <Grid key={value} item>
+                            <Paper className={classes.paper} />
+                        </Grid>
+                    ))}
+                </Grid>
+            ) : (
+                <div className={styles.noWidgetContainer}>
+                    <div className={styles.message}>Your focus page is empty</div>
+                    <div className={styles.addWidget}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            classes={{ root: classes.root, label: classes.label }}
+                        >
+                            Add Widget
+                        </Button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
